bfcbe717798d23ba5945820a5d704fd5a93053bc
barsandcat <barsandcat@gmail.com>
2024-08-23 22:48:57 +0200

    Use common game instance and common player controller

---
+++ Source/ShipEditor/Private/MyGameUIManagerSubsystem.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2024, sillygilly. All rights reserved.
+
+
+#include "MyGameUIManagerSubsystem.h"
+
---
+++ Source/ShipEditor/Public/MyGameUIManagerSubsystem.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, sillygilly. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameUIManagerSubsystem.h"
+#include "MyGameUIManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHIPEDITOR_API UMyGameUIManagerSubsystem : public UGameUIManagerSubsystem
+{
+	GENERATED_BODY()
+	
+};
