4f5263de9a63e63507c3ff9941c4e547270e49d9
barsandcat <barsandcat@gmail.com>
2024-11-18 21:48:46 +0100

    Add ship part browser widget

---
+++ Source/ShipEditor/Public/UI/Widgets/ShipPartBrowserActivatableWidget.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, sillygilly. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "ShipPartBrowserActivatableWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHIPEDITOR_API UShipPartBrowserActivatableWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
