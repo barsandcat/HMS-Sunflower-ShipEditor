fa9d04265440b2ca3e03117f97c7b72e35163b90
barsandcat <barsandcat@gmail.com>
2025-03-09 22:46:42 +0100

    Bind view model to the SetOptions function.

--- Source/ShipEditor/Private/UI/Widgets/MyComboBoxKey.cpp
+++ Source/ShipEditor/Private/UI/Widgets/MyComboBoxKey.cpp
@@ -1,5 +1,12 @@
 // Copyright (c) 2025, sillygilly. All rights reserved.
 
-
 #include "UI/Widgets/MyComboBoxKey.h"
 
+void UMyComboBoxKey::SetOptions(const TArray<FName>& options_array)
+{
+	ClearOptions();
+	for (const FName& option : options_array)
+	{
+		AddOption(option);
+	}
+}
--- Source/ShipEditor/Public/UI/Widgets/MyComboBoxKey.h
+++ Source/ShipEditor/Public/UI/Widgets/MyComboBoxKey.h
@@ -2,16 +2,16 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/ComboBoxKey.h"
+#include "CoreMinimal.h"
+
 #include "MyComboBoxKey.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SHIPEDITOR_API UMyComboBoxKey : public UComboBoxKey
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetOptions(const TArray<FName>& options_array);
 };
