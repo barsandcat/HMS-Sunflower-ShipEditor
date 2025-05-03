5057f2a505ee41a1916d3e7bdf14b78bc71ab6b7
barsandcat <barsandcat@gmail.com>
2025-05-03 18:02:24 +0200

    File dialog now opens shipplans directory
    Add get opacity conversion function

--- Source/ShipEditor/Private/UI/ViewModels/VMFileSystem.cpp
+++ Source/ShipEditor/Private/UI/ViewModels/VMFileSystem.cpp
@@ -16,19 +16,40 @@ FString UVMFileSystem::GetCWD() const
 
 TArray<UVMFileName*> UVMFileSystem::GetFiles() const
 {
-	IFileManager& FileManager = IFileManager::Get();
-
-	FString path = "*.*";
-	TArray<FString> output;
-	FileManager.FindFiles(output, *path, true, true);
-	TArray<UVMFileName*> files;
-	for (const FString& file : output)
+	class FShipPlansVisitor : public IPlatformFile::FDirectoryVisitor
 	{
-		UVMFileName* fileName = NewObject<UVMFileName>();
-		fileName->SetName(file);
-		fileName->SetIsDirectory(false);
-		files.Add(fileName);
-	}
+	public:
+		TArray<UVMFileName*> Result;
 
-	return files;
+		virtual bool Visit(const TCHAR* FilenameOrDirectory, bool is_directory)
+		{
+			FString file_name = FPaths::GetCleanFilename(FilenameOrDirectory);
+			if (!is_directory)
+			{
+				if (FPaths::GetExtension(file_name, false).ToLower() != TEXT("shipplan"))
+				{
+					return true;
+				}
+				else
+				{
+					file_name = FPaths::GetBaseFilename(file_name);
+				}
+			}
+			UVMFileName* vm = NewObject<UVMFileName>();
+			vm->SetName(file_name);
+			vm->SetIsDirectory(is_directory);
+			Result.Add(vm);
+			return true;
+		}
+	};
+
+	IFileManager& file_manager = IFileManager::Get();
+
+	FString path = FPaths::Combine(FPaths::ProjectSavedDir(), TEXT("ShipPlans"));
+	UE_LOG(LogTemp, Warning, TEXT("path: %s"), *path);
+	file_manager.MakeDirectory(*path, true);
+	FShipPlansVisitor visitor;
+	file_manager.IterateDirectory(*path, visitor);
+
+	return visitor.Result;
 }
